e6c7f6a8dc5b0b33dc0c2538c03d7b745d5030f8
miyamotoh30 <miyamotoh30@icloud.com>
2024-08-05 15:55:15 +0900

    Add files via upload
    商品一覧ページを追加
    商品詳細ページ作成中

--- ccdonuts/index.php
+++ ccdonuts/index.php
@@ -5,72 +5,85 @@
         <p>ようこそ&nbsp;ゲスト様</p>
     </section>
     <section class="heroImage">
-        <h1><img src="images/heroImage.png" alt="ccdonuts"></h1>
+        <div class="container">
+            <h1><img srcset="images/heroImage.png 320w,images/pcHeroImage.png 1280w"
+             src="images/heroImage.png" alt="ccdonuts"></h1>
+        </div><!--container-->
     </section>
     <section class="information">
         <article class="newItem">
-            <h2><img src="images/citrus.png" alt="newItem"></h2>
+            <h2><img srcset="images/citrus.png 320w,images/pcNewItem.png 1280w" 
+            src="images/citrus.png" alt="newItem"></h2>
             <p class="newBadge">新製品</p>
             <p class="itemname">サマーシトラス</p>
         </article>
         <article class="lifeWithDonuts">
-            <h2><img src="images/lifeImage.png" alt="newItem"></h2>
+            <h2><img srcset="images/lifeImage.png 320w,images/pcLifeImage.png 1280w" src="images/lifeImage.png" alt="newItem"></h2>
             <p class="title">ドーナツのある生活</p>
         </article>
         <article class="allItems">
-            <h2><img src="images/allItembaner.png" alt="newItem"></h2>
+            <h2><img srcset="images/allItembaner.png 320w,images/pcAllItembaner.png 1280w"
+            src="images/pcAllItembaner.png" alt="newItem"></h2>
             <p class="banerTitle">商品一覧</p>
         </article>
     </section>
     <section class="philosophy">
-        <div class="backgroundPhoto">
-            <h2>Philosophy</h2>
-            <h3>私たちの信念</h3>
-            <p class="enText">"Creating Connections"</p>
-            <p class="jpText">「ドーナツでつながる」</p>
-        </div>
+        <div class="container">
+            <div class="backgroundPhoto">
+                <h2>Philosophy</h2>
+                <h3>私たちの信念</h3>
+                <p class="enText">"Creating Connections"</p>
+                <p class="jpText">「ドーナツでつながる」</p>
+            </div>
+        </div><!--container-->
     </section>
     <section class="ranking">
         <h2>人気ランキング</h2>
         <div class="rankingContainer">
             <article>
                 <p class="rankingNumber topItem">1</p>
-                <h3 class="rankingPhoto"><img src="images/ranking01.png" alt="ranking01"></h3>
+                <h3 class="rankingPhoto"><img srcset="images/ranking01.png 320w,images/pcRanking01.png 1280w" 
+                src="images/ranking01.png" alt="ranking01"></h3>
                 <p class="rankingItem">CCドーナツ 当店オリジナル（5個入り）</p>
                 <p class="price">税込 &nbsp;&nbsp; ￥1,500</p>
                 <button class="buyItem">カートに入れる</button>
             </article>
             <article>
                 <p class="rankingNumber secondItem">2</p>
-                <h3 class="rankingPhoto"><img src="images/ranking02.png" alt="ranking02"></h3>
+                <h3 class="rankingPhoto"><img srcset="images/ranking02.png 320w,images/pcRanking02.png 1280w" 
+                src="images/ranking02.png" alt="ranking02"></h3>
                 <p class="rankingItem">フルーツドーナツセット（12個入り）</p>
                 <p class="price">税込 &nbsp;&nbsp; ￥3,500</p>
                 <button class="buyItem">カートに入れる</button>
             </article>
             <article>
                 <p class="rankingNumber thirdItem">3</p>
-                <h3 class="rankingPhoto"><img src="images/ranking03.png" alt="ranking03"></h3>
+                <h3 class="rankingPhoto"><img srcset="images/ranking03.png 320w,images/pcRanking03.png 1280w" 
+                src="images/ranking03.png" alt="ranking03"></h3>
                 <p class="rankingItem">フルーツドーナツセット（14個入り）</p>
                 <p class="price">税込 &nbsp;&nbsp; ￥4,000</p>
                 <button class="buyItem">カートに入れる</button>
             </article>
             <article>
                 <p class="rankingNumber">4</p>
-                <h3 class="rankingPhoto"><img src="images/ranking04.png" alt="ranking04"></h3>
+                <h3 class="rankingPhoto"><img srcset="images/ranking04.png 320w,images/pcRanking04.png 1280w" 
+                src="images/ranking04.png" alt="ranking04"></h3>
                 <p class="rankingItem">チョコレートデライト（5個入り）</p>
                 <p class="price">税込 &nbsp;&nbsp; ￥1,600</p>
                 <button class="buyItem">カートに入れる</button>
             </article>
             <article>
                 <p class="rankingNumber">5</p>
-                <h3 class="rankingPhoto"><img src="images/ranking05.png" alt="ranking05"></h3>
+                <h3 class="rankingPhoto"><img srcset="images/ranking05.png 320w,images/pcRanking05.png 1280w" 
+                src="images/ranking05.png" alt="ranking05"></h3>
                 <p class="rankingItem">ベストセレクションボックス（4個入り）</p>
                 <p class="price">税込 &nbsp;&nbsp; ￥1,200</p>
                 <button class="buyItem">カートに入れる</button>
             </article>
             <article>
                 <p class="rankingNumber">6</p>
-                <h3 class="rankingPhoto"><img src="images/ranking06.png" alt="ranking06"></h3>
+                <h3 class="rankingPhoto"><img srcset="images/ranking06.png 320w,images/pcRanking06.png 320w" 
+                src="images/ranking06.png" alt="ranking06"></h3>
                 <p class="rankingItem">ストロベリークラッシュ（5個入り）</p>
                 <p class="price">税込 &nbsp;&nbsp; ￥1,800</p>
                 <button class="buyItem">カートに入れる</button>
---
+++ ccdonuts/itemdetail.php
@@ -0,0 +1,30 @@
+<?php require 'header.php';?>
+<link rel="stylesheet" href="styles/items.css">
+<main>
+    <div class="breadclumb">
+        <p><a href="index.php">Top</a> > 商品一覧 </p>
+    </div>
+    <div class="username">
+        <p>ようこそ&nbsp;ゲスト様</p>
+    </div>
+    <div class="container">
+        <h1><img src="images/ccdonuts.png" alt="ccdonuts"></h1>
+        <h2>CCドーナツ 当店オリジナル（5個入り）</h2>
+        <p class="description">
+            当店のオリジナル商品、CCドーナツは、サクサクの食感が特徴のプレーンタイプのドーナツです。
+            素材にこだわり、丁寧に揚げた生地は軽やかでサクッとした食感が楽しめます。
+            一口食べれば、口の中に広がる甘くて香ばしい香りと、口どけの良い食感が感じられます。
+        </p>
+        <p class="price">税込 &nbsp;&nbsp; ￥1,500</p>
+        <form method="post" action="cart.php">
+            <p><input type="text" name="count">個</p>
+            <input type="submit" value="カートに入れる" class="buyItem">
+            <button name="favorite" class="favorite">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                </svg>
+            </button>
+        </form>
+    </div>
+</main>
+<?php require 'footer.php';?>

e1de1094619d11d7363f9a4e5aaf1cb76fc79d52
miyamotoh30 <miyamotoh30@icloud.com>
2024-09-10 15:49:34 +0900

    Add files via upload
    ランダムにアニメーションを設定する関数を追加。

--- hideandseek/scripts/game.js
+++ hideandseek/scripts/game.js
@@ -2,9 +2,21 @@
 
 /* ペンギンをクリックしたとき*/
 let penguins = Array.from(document.getElementsByClassName('penguin'));
+const maxPattern = 3;/* アニメーションパターンを3とする */
+const maxSec = 10;/* アニメーションを最大10秒とする */
 penguins.forEach(function(element){
     element.addEventListener('click',function(){
-        //console.log(element);
         document.getElementById('clearGame').style.display='block';
     });    
 });
+
+function setAnimation(penguin){
+    const second = getRandomInt(maxSec);
+    const type = getRandomInt(maxPattern);
+    const paramator = `hideandseek${type} ${seccond}s ease-in-out`;
+    penguin.style.animation= paramator;    
+}
+
+function getRandomInt(max){
+    return Math.floor(Math.random*max);
+}

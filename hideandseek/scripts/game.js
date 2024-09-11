d9e4283136ec9cbbfb33e053c4bed8a7fc9fa351
miyamotoh30 <miyamotoh30@icloud.com>
2024-09-11 14:10:56 +0900

    Add files via upload
    一応はゲームになったので、一旦コミットしました。

--- hideandseek/scripts/game.js
+++ hideandseek/scripts/game.js
@@ -1,22 +1,50 @@
 'use stricts';
 
-/* ペンギンをクリックしたとき*/
 let penguins = Array.from(document.getElementsByClassName('penguin'));
-const maxPattern = 3;/* アニメーションパターンを3とする */
-const maxSec = 10;/* アニメーションを最大10秒とする */
+/* ペンギンをクリックしたときのイベントを設定する*/
 penguins.forEach(function(element){
     element.addEventListener('click',function(){
-        document.getElementById('clearGame').style.display='block';
+        gameSet();
     });    
 });
-
+function gameSet(){
+    document.getElementById('gameStartMessage').style.display='none';
+    document.getElementById('clearGame').style.display='block';
+    penguins.forEach(function(element){
+        element.style.display='none';
+    });
+}
+const maxPattern = 3;/* アニメーションパターンを3とする */
 function setAnimation(penguin){
-    const second = getRandomInt(maxSec);
-    const type = getRandomInt(maxPattern);
-    const paramator = `hideandseek${type} ${seccond}s ease-in-out`;
-    penguin.style.animation= paramator;    
+    //console.log(penguin);
+    const act = `animation${getRandomInt(maxPattern)}`;
+    //console.log(act);
+    penguin.classList.add(act);
 }
 
 function getRandomInt(max){
-    return Math.floor(Math.random*max);
+    return Math.floor(Math.random()*max);
+}
+/* テストのための仮の値設定+*/
+const sec=3000;/*3秒ごとにアニメーションをセットする*/
+let i = 0; /* setIntervalを管理するためのカウンター */
+let id = 0;/* setIntervalを終了するためのid */
+
+window.addEventListener('load',function(){
+    console.log('window load');/* ページが読み込み終わったのを確認 */
+    id = this.setInterval(setMove,sec); /*3秒ごとにアニメーションをセットし、アニメーションの開始をずらす*/
+    //for(i=0;i<=penguins.length;i++){
+        //setAnimation(penguins[i]); /*ペンギンにアニメーションを設定する */
+    //}
+});
+/* アニメーションをセット*/
+let setMove = function(){
+    console.log(`${i}回目`);
+    if(i>=penguins.length){
+        clearInterval(id);
+        console.log('setInterval終了');
+    }else{
+        setAnimation(penguins[i]);
+    }
+    i++;
 }
